80100683a8119c1c376bbc044103f549f7387df8
Hussain-Safwan <safwan.du16@gmail.com>
2020-02-09 16:57:07 +0600

    writing optimised, not so much tho :-(

--- app.js
+++ app.js
@@ -28,16 +28,15 @@ app.use(cookieParser());
 
 // Express Session
 app.use(
-  session({
-    secret: "secret_KEY",
-    saveUninitialized: true,
-    resave: true
-  })
+    session({
+        secret: "secret_KEY",
+        saveUninitialized: true,
+        resave: true
+    })
 );
 
-mongoose.connect( keys.cred.mongoClientID,
-  { useNewUrlParser: true },
-  () => console.log("atlas db connected")
+mongoose.connect(keys.cred.mongoClientID, { useNewUrlParser: true },
+    () => console.log("atlas db connected")
 );
 
 const port = 3001
@@ -45,69 +44,96 @@ server.listen(port);
 console.log("server running at ", port);
 
 app.get("/", (req, res) => {
-  const id = '5e243df41d878d16693da671';
-  sourceCodeModule.findOne({_id: id}).then(foundPost => {
-    res.render('index', {
-      dbSource: foundPost.codeBody,
-      trig: foundPost.trigger
-    })
-  });
-  
+    const id = '5e243df41d878d16693da671';
+    sourceCodeModule.findOne({ _id: id }).then(foundPost => {
+        res.render('index', {
+            dbSource: foundPost.codeBody,
+            trig: foundPost.trigger
+        })
+    });
+
 });
 
 io.sockets.on("connection", socket => {
-  
-  //Connect
-  connections.push(socket);
-  console.log("connected: ", connections.length);
 
-  //Disconnect
-  socket.on("disconnect", data => {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log("disconnected! left:  ", connections.length);
-  });
+    //Connect
+    connections.push(socket);
+    console.log("connected: ", connections.length);
 
-  //Send Message
-  socket.on('send message', data => {
-    const id = '5e243df41d878d16693da671';
-    sourceCodeModule.updateOne({ _id: id }, { $set: {codeBody: data} }, (err, docs) => {
-        
-     })
-    
-  });
-
-  //Change trigger
-  socket.on('trigger change', data => {
-
-    const id = '5e243df41d878d16693da671';
-    sourceCodeModule.updateOne({ _id: id }, { $set: {trigger: data} }, (err, docs) => {
-        
-     })
-    
-  });
-
-  //Watch cahnges
-  sourceCodeModule.watch().
-  on('change', data => {
-
-    let codeString = data.updateDescription.updatedFields.codeBody;
-    let trigger = data.updateDescription.updatedFields.trigger
-    if (trigger == null){
-      io.sockets.emit('new message', {msg: codeString});
-    }
-    if (codeString == null) {
-      io.sockets.emit('trigger change', {trig: trigger})
-    }
-  });
-
-  //Upload to database
-  function upload(codeBody) {
-    const updatedSource = new sourceCodeModule ({
-      codeBody
+    //Disconnect
+    socket.on("disconnect", data => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("disconnected! left:  ", connections.length);
     });
-    updatedSource.save().then((err, post) => {
-      console.log('uploaded!');
+
+    //Send Message
+    socket.on('send message', data => {
+
+        const path = '/home/mah-nigga/Projects/RealTime-Edtior/data.txt'
+        write_file(path, data)
     });
-  }
+
+    //Change trigger
+    socket.on('trigger change', data => {
+
+        const id = '5e243df41d878d16693da671';
+        sourceCodeModule.updateOne({ _id: id }, { $set: { trigger: data } }, (err, docs) => {
+
+        })
+
+    });
+
+    //Watch cahnges
+    sourceCodeModule.watch().
+    on('change', data => {
+
+        let codeString = data.updateDescription.updatedFields.codeBody;
+        let trigger = data.updateDescription.updatedFields.trigger
+        if (codeString == null) {
+            io.sockets.emit('trigger change', { trig: trigger })
+        }
+    });
+
+    //Upload to database
+    function upload(codeBody) {
+        const updatedSource = new sourceCodeModule({
+            codeBody
+        });
+        updatedSource.save().then((err, post) => {
+            console.log('uploaded!');
+        });
+    }
 
 });
+
+setTimeout(() => {
+    const id = '5e243df41d878d16693da671';
+    const path = '/home/mah-nigga/Projects/RealTime-Edtior/data.txt'
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        sourceCodeModule.updateOne({ _id: id }, { $set: { codeBody: data } }, (err, docs) => {
+
+        })
+    })
+}, 2000)
+
+const fs = require('fs')
+fs.watchFile('/home/mah-nigga/Projects/RealTime-Edtior/data.txt', { interval: 5000 }, async(curr, prev) => {
+    const path = '/home/mah-nigga/Projects/RealTime-Edtior/data.txt'
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        io.sockets.emit('new message', { msg: data });
+    })
+});
+
+function write_file(path, data) {
+    fs.writeFile(path, data, function(err) {
+        if (err) throw err;
+    });
+}
